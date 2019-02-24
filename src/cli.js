9cab8dabee95927e1d6b83951385ccbbdc8d2251
Mircea Badragan <badraganmircea@gmail.com>
2019-02-24 17:14:35 +0200

    wip on cli

---
+++ src/cli.js
@@ -0,0 +1,93 @@
+const readline = require('readline');
+const util = require('util');
+const debug = util.debuglog('cli');
+var events = require('events');
+
+const {
+  lstatSync,
+  readdirSync
+} = require('fs');
+const {
+  join
+} = require('path');
+
+
+const isDirectory = source => lstatSync(source).isDirectory()
+const getDirectories = source =>
+  readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+class _events extends events {};
+
+const e = new _events();
+
+const cli = {};
+
+cli.processInput = function(str) {
+  str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : false;
+  if (!str) return;
+  const uniqueInputs = [
+    'help',
+    'exits',
+    'listversions'
+  ];
+
+  let matchedFount = false;
+  let matchCount = 0;
+
+  uniqueInputs.some(function(input) {
+    if (str.toLowerCase().indexOf(input) > -1) {
+      matchedFount = true;
+      e.emit(input, str);
+      return true;
+    }
+  })
+
+  if (!matchedFount) {
+    console.log('specified command does not exist');
+  }
+};
+
+e.on('help', function(str) {
+  cli.responders.help();
+});
+
+e.on('exit', function(str) {
+  cli.responders.exit();
+});
+
+e.on('listversions', function(str) {
+  cli.responders.listVersions();
+})
+
+cli.responders = {};
+
+cli.responders.help = function() {
+  console.log('you asked for help..');
+}
+
+cli.responders.listVersions = function() {
+  console.log(getDirectories('./mutations'));
+}
+
+cli.init = function() {
+  console.log('\x1b[34m%s\x1b[0m', 'the cli is running');
+
+  const _interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
+  });
+
+  _interface.prompt();
+
+  _interface.on('line', function(str) {
+    cli.processInput(str);
+    _interface.prompt();
+  })
+
+  _interface.on('close', function() {
+    process.exit(0);
+  })
+}
+
+module.exports = cli;
--- src/index.js
+++ src/index.js
@@ -3,57 +3,66 @@ const inputConfig = require('./input/inputConfig.json');
 const mutationConfig2 = require('./input/mutationsV2.json');
 const mutationConfig3 = require('./input/mutationsV3.json');
 const mutationConfig4 = require('./input/mutationsV4.json');
+const cli = require('./cli');
 
 // const mutationsList = [mutationConfig2, mutationConfig3, mutationConfig4];
 const mutationsList = [mutationConfig2, mutationConfig3];
 
 const mutationTypes = {
-    ADD: 'ADD',
-    RENAME: 'RENAME',
-    MOVE: 'MOVE',
-    DELETE: 'DELETE'
+  ADD: 'ADD',
+  RENAME: 'RENAME',
+  MOVE: 'MOVE',
+  DELETE: 'DELETE'
 };
 
 const mutationActions = {
-    [mutationTypes.RENAME]: (comp, definition) => {
-        comp.renameKeys(definition.from, definition.to);
-    },
-    [mutationTypes.MOVE]: (comp, definition) => {
-        comp.moveKey(definition.from, definition.to);
-    },
-    [mutationTypes.DELETE]: (comp, definition) => {
-        comp.deleteKey(definition.from);
-    },
-    [mutationTypes.ADD]: (comp, definition) => {
-        comp.addToKey(definition.to, definition.targetObject);
-    }
+  [mutationTypes.RENAME]: (comp, definition) => {
+    comp.renameKeys(definition.from, definition.to);
+  },
+  [mutationTypes.MOVE]: (comp, definition) => {
+    comp.moveKey(definition.from, definition.to);
+  },
+  [mutationTypes.DELETE]: (comp, definition) => {
+    comp.deleteKey(definition.from);
+  },
+  [mutationTypes.ADD]: (comp, definition) => {
+    comp.addToKey(definition.to, definition.targetObject);
+  }
 };
 
 const mutate = (componentConfig, mutationConfig) => {
-    const {components} = componentConfig;
-    return components.map(comp => {
-        const mutations = mutationConfig.mutations[comp.type];
-        const resultingComponent = {...comp};
+  const {
+    components
+  } = componentConfig;
+  return components.map(comp => {
+    const mutations = mutationConfig.mutations[comp.type];
+    const resultingComponent = {
+      ...comp
+    };
 
-        if (!mutations) {
-            return comp;
-        }
+    if (!mutations) {
+      return comp;
+    }
 
-        mutations.forEach(mutation => {
-            mutationActions[mutation.mutationType](resultingComponent, mutation.definition);
-        });
+    mutations.forEach(mutation => {
+      mutationActions[mutation.mutationType](resultingComponent, mutation.definition);
+    });
 
-        return comp;
-    })
+    return comp;
+  })
 };
 
 const migrate = (inputConfig, mutationsList) => {
-    let inProgressConfig = {...inputConfig};
-    mutationsList.forEach((mutation) => {
-        inProgressConfig.components = mutate(inProgressConfig, mutation);
-        console.log('config at version => ', mutation.version);
-        console.log('in progress config', JSON.stringify(inProgressConfig, null, 2));
-    });
+  let inProgressConfig = {
+    ...inputConfig
+  };
+  mutationsList.forEach((mutation) => {
+    inProgressConfig.components = mutate(inProgressConfig, mutation);
+    console.log('config at version => ', mutation.version);
+    console.log('in progress config', JSON.stringify(inProgressConfig, null, 2));
+  });
 };
 
-migrate(inputConfig.configuration, mutationsList);
+// migrate(inputConfig.configuration, mutationsList);
+
+cli.init();
