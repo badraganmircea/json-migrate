232b131988e15062ec8686fc5a38377e41a6a469
Mircea Badragan <badraganmircea@gmail.com>
2019-04-04 14:20:55 +0300

    added 2 more use cases for version lists

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,5 +3,7 @@ const mutateUtils = require('../utils');
 describe('utils test suite', () => {
   it('createInBetweenVersionsArr', () => {
     expect(mutateUtils.createInBetweenVersionsArr(2, 5)).toEqual([2, 3, 4, 5]);
+    expect(mutateUtils.createInBetweenVersionsArr(2, 2)).toEqual([2]);
+    expect(mutateUtils.createInBetweenVersionsArr(1, 2)).toEqual([1, 2]);
   })
 });
